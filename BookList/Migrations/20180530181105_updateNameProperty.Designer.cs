15a76467348e85858d896937975c87c1f180c037
Mariusz Śliwa <ms204@wp.pl>
2018-05-30 23:12:37 +0200

    Działające dodawanie, usuwanie, edytowanie książek

--- BookList/Migrations/20180530181105_updateNameProperty.Designer.cs
+++ BookList/Migrations/20180530181105_updateNameProperty.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace BookList.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180530181105_updateNameProperty")]
+    partial class updateNameProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
