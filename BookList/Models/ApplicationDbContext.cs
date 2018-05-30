eeaf469701211fc3b903cc635cc8cb1928e0ad9a
Mariusz Śliwa <ms204@wp.pl>
2018-05-30 03:22:39 +0200

    added Db

--- BookList/Models/ApplicationDbContext.cs
+++ BookList/Models/ApplicationDbContext.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BookList.Models
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
 
+        public DbSet<Book> Books { get; set; }
     }
 }
